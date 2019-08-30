1f339cd473554683d7f333b9d7d1625fa3c733de
Aubbakr <realchampiontheno1@gmail.com>
2019-08-30 12:18:44 +0500

    Styled About and 404 Page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,12 +1,16 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
-function About(props) {
+function About() {
   return (
-    <Fragment>
-      <h1>About this App</h1>
-      <p>Search Github Users using the Github REST API.</p>
-      <p>Version: 1.0.0</p>
-    </Fragment>
+    <section className='rounded-lg shadow-lg bg-white py-8 px-16 my-12'>
+      <h2 className='text-4xl'>About this App</h2>
+      <p className='text-gray-700 text-lg'>
+        Search Github Users using the Github REST API.
+      </p>
+      <p className='text-gray-700 text-lg'>
+        <strong>Version:</strong> 1.0.0
+      </p>
+    </section>
   );
 }
 
--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -3,13 +3,18 @@ import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
-    <div>
-      <h1>Not Found</h1>
-      <p className='lead'>The page you're looking for does not exist...</p>
-      <Link to='/' className='btn btn-dark'>
+    <section className='rounded-lg shadow-lg bg-white py-8 px-16 my-12'>
+      <h2 className='text-4xl'>Page Not Found</h2>
+      <p className='text-gray-700 text-lg mb-2'>
+        The page you're looking for does not exist...
+      </p>
+      <Link
+        to='/'
+        className='inline-block bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 mt-6 rounded'
+      >
         Back to Search
       </Link>
-    </div>
+    </section>
   );
 }
 
