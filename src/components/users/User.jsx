f6ee93f9cd14587985531e867801d0e486da9902
Aubbakr <realchampiontheno1@gmail.com>
2019-08-30 11:22:52 +0500

    Styled User Page

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -17,10 +17,16 @@ function User({ match }) {
 
   const isHirableIcon = hireable => {
     return hireable ? (
-      <i className='fas fa-check text-success' />
+      <i className='fas fa-check text-green-500' />
     ) : (
-      <i className='fas fa-times-circle text-danger' />
+      <i className='fas fa-times-circle text-red-500' />
     );
+  };
+
+  const isAvailable = info => {
+    if (info) return <span className='text-gray-800'>{info}</span>;
+
+    return <span className='text-gray-600'>Not Available</span>;
   };
 
   const {
@@ -43,61 +49,73 @@ function User({ match }) {
 
   return (
     <Fragment>
-      <Link to='/' className='btn btn-light'>
-        Back to Search
+      <Link to='/' className='inline-block font-bold my-6 hover:text-blue-500'>
+        <i className='fas fa-long-arrow-alt-left pr-1' /> Back to Search
       </Link>
-      <strong>Hireable:</strong> {isHirableIcon(hireable)}
-      <div className='card grid-2'>
-        <div className='all-center'>
+
+      <section className='flex items-start rounded-lg shadow-lg text-gray-900 bg-white py-12 pr-24 mb-6'>
+        <section className='w-2/5 text-center'>
           <img
             src={avatar_url}
             alt={name}
-            className='round-img'
-            style={{ width: '150px' }}
+            className='rounded-full w-1/2 mx-auto mb-3'
           />
-          <h1>{name}</h1>
-          <p>Location: {location}</p>
-        </div>
-        <div>
-          {bio && (
-            <Fragment>
-              <h1>Bio</h1>
-              <p>{bio}</p>
-            </Fragment>
-          )}
+          <h1 className='font-bold text-2xl mb-1'>{name}</h1>
+          <p className='text-gray-900 text-sm'>
+            <i className='fas fa-map-marker-alt text-blue-500 mr-1' />
+            {isAvailable(location)}
+          </p>
+        </section>
+
+        <section className='w-3/5'>
+          <h2 className='text-xl font-bold'>Bio</h2>
+          <p className='text-gray-800'>{isAvailable(bio)}</p>
+
           <a
             href={html_url}
             target='_blank'
             rel='noopener noreferrer'
-            className='btn btn-dark my-1'
+            className='inline-block rounded bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 my-6'
           >
             Github Profile
           </a>
 
-          <ul>
-            {login && (
-              <li>
-                <b>Username:</b> {login}
-              </li>
-            )}
-            {company && (
-              <li>
-                <b>Company:</b> {company}
-              </li>
-            )}
-            {blog && (
-              <li>
-                <b>Website:</b> {blog}
-              </li>
-            )}
-          </ul>
-        </div>
-      </div>
-      <div className='card text-center'>
-        <span className='badge badge-primary'>Followers: {followers}</span>
-        <span className='badge badge-success'>Following: {following}</span>
-        <span className='badge badge-light'>Public Repos: {public_repos}</span>
-        <span className='badge badge-dark'>Public Gists: {public_gists}</span>
+          <div
+            style={{
+              display: 'grid',
+              gridTemplateColumns: 'max-content 1fr',
+              gridColumnGap: '0.5rem'
+            }}
+            className='text-sm'
+          >
+            <span className='font-bold'>Username:</span>
+            <span>{login}</span>
+
+            <span className='font-bold'>Company:</span>
+            {isAvailable(company)}
+
+            <span className='font-bold'>Website:</span>
+            {isAvailable(blog)}
+
+            <span className='font-bold'>Hireable:</span>
+            <span>{isHirableIcon(hireable)}</span>
+          </div>
+        </section>
+      </section>
+
+      <div className='flex justify-center rounded-lg shadow-lg text-xs text-white font-bold bg-white py-6 mb-6'>
+        <span className='bg-teal-500 py-1 px-3 mr-2 rounded-full'>
+          {followers} Followers
+        </span>
+        <span className='bg-green-500 py-1 px-3 mr-2 rounded-full'>
+          {following} Following
+        </span>
+        <span className='bg-blue-500 py-1 px-3 mr-2 rounded-full'>
+          {public_repos} Public Repos
+        </span>
+        <span className='bg-purple-500 py-1 px-3 mr-2 rounded-full'>
+          {public_gists} Public Gists
+        </span>
       </div>
       <Repos repos={repos} />
     </Fragment>
--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+
+ReactDOM.render(<App />, rootEl);
